a48df6ebb450e79101790bd291461dd19c428677
Lawrence Caulker <locaulker@gmail.com>
2026-05-30 14:25:27 -0400

    installed shadcn

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,4 +1,14 @@
+import { Button } from "@/components/ui/button"
+import { Input } from "@/components/ui/input"
+
 function Homepage() {
-  return <div className="p-5">Homepage</div>
+  return (
+    <div className="p-5 flex flex-col gap-5 w-max">
+      <h1>Homepage</h1>
+      <Button>Shadcn Default Button</Button>
+
+      <Input placeholder="Shadcn Default Input" />
+    </div>
+  )
 }
 export default Homepage
